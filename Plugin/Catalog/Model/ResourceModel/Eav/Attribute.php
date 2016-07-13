8a8522b5faffde92154a03d11307f4c9ba834e1e
Gordon Lesti <info@gordonlesti.com>
2016-07-13 16:32:04 +0200

    update

--- Plugin/Catalog/Model/ResourceModel/Eav/Attribute.php
+++ Plugin/Catalog/Model/ResourceModel/Eav/Attribute.php
@@ -7,15 +7,17 @@
  */
 namespace Part\Decimal\Plugin\Catalog\Model\ResourceModel\Eav;
 
+use Part\Decimal\Model\Decimal;
+
 class Attribute
 {
     public function beforeSave(\Magento\Catalog\Model\ResourceModel\Eav\Attribute $subject)
     {
         $data = $subject->getData();
-        if (isset($data['frontend_input']) &&
-            $data['frontend_input'] === \Part\Decimal\Model\Decimal::DECIMAL_TYPE_ATTRIBUTE_FRONTEND_INPUT) {
+        if (isset($data[Decimal::DECIMAL_INPUT_TYPE_KEY]) &&
+            $data[Decimal::DECIMAL_INPUT_TYPE_KEY] === Decimal::DECIMAL_TYPE_ATTRIBUTE_FRONTEND_INPUT) {
             $data['backend_type'] = 'decimal';
-            $data['frontend_input_renderer'] = '\Magento\Framework\Data\Form\Element\Text';
+            $data['frontend_input_renderer'] = 'Magento\Framework\Data\Form\Element\Text';
             $subject->setData($data);
         }
     }
